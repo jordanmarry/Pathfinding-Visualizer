f9e196094246e79553069f76f5c08788a90c2708
Jordan Marry <jmarry@terpmail.umd.edu>
2021-05-23 23:48:38 -0400

    Commiting Code and such

---
+++ pathfinding/src/PathfindingVisual/PathfindingVisual.jsx
@@ -0,0 +1,177 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
+
+import './PathfindingVisual.css';
+
+/* */
+/* */
+/* Figure out how to change the start node and finish node */
+/* */
+/* */
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
+
+/* Class Pathfinding creates the grid itself and sets up mouseIsPressed */
+
+export default class PathfindingVisual extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
+
+  /* .setState is setting the state in the constructor under this.state */
+  /* This also initilizes the state of the grid itself. */
+
+  componentDidMount() {
+    const grid = [];
+    for (let row = 0; row < 15; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 40; col++) {
+        currentRow.push(createNode(row, col));
+      }
+      grid.push(currentRow);
+    }
+    this.setState({grid});
+
+  }
+
+  /* When you click your mouse down it changes the node if theres a wall / no wall */
+
+  handleMouseDown(row, col) {
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({grid: newGrid, mouseIsPressed: true});
+  }
+
+  /* This is for when you hold the mouse button down changes to the wall / no wall */
+
+  // THIS IS THE ISSUE WITH THE ANIMATION
+
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed){
+      return;
+    }
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({grid: newGrid, mouseIsPressed: true});
+  }
+
+  /* Changes when you release the mouse */
+
+  handleMouseUp() {
+    this.setState({mouseIsPressed: false});
+  }
+
+  /*  */
+
+  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-visited';
+      }, 10 * i);
+    }
+  }
+
+  /*  */
+
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-shortest-path';
+      }, 50 * i);
+    }
+  }
+
+  /*  */
+
+  visualizeDijkstra() {
+    const {grid} = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+  }
+
+  /*  */
+
+  render() {
+    const {grid, mouseIsPressed} = this.state;
+
+    return (
+      <>
+        <button onClick={() => this.visualizeDijkstra()}>
+          Visualize Dijkstra's Algorithm
+        </button>
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const {row, col, isFinish, isStart, isWall} = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      col={col}
+                      isFinish={isFinish}
+                      isStart={isStart}
+                      isWall={isWall}
+                      mouseIsPressed={mouseIsPressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) => this.handleMouseEnter(row, col)}
+                      onMouseUp={() => this.handleMouseUp()}
+                      row={row}></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
+
+/* This creates a new node for every single index. Says if is start/finish figures out the distance/visited/wall */
+
+const createNode = (row, col) => {
+  return {
+    row,
+    col,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+
+/* Changes the node to the wall or not */
+/* CHANGE TO FIGURE OUT HOW TO ANIMATE */ 
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
---
+++ pathfinding/src/algorithms/dijkstra.js
@@ -0,0 +1,14 @@
+// Performs Dijkstra's algorithm; returns *all* nodes in the order
+// in which they were visited. Also makes nodes point back to their
+// previous node, effectively allowing us to compute the shortest path
+// by backtracking from the finish node.
+export function dijkstra(grid, startNode, finishNode) {
+    
+}
+  
+
+  // Backtracks from the finishNode to find the shortest path.
+  // Only works when called *after* the dijkstra method above.
+export function getNodesInShortestPathOrder(finishNode) {
+    
+}

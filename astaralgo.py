bb614e6f8815f4107a94dbb42b3d7c35c1332ac3
Jordan Marry <jmarry@terpmail.umd.edu>
2021-06-12 12:22:34 -0400

    Changed Code Around

--- astaralgo.py
+++ astaralgo.py
@@ -15,11 +15,30 @@ def reconstruct(prevNodes, curr, draw):
         draw()
 
 
+def updateNeighbor(node, grid):
+    node.neighbors = []
+    # Goes Down
+    if node.row < node.total_rows - 1 and not grid[node.row + 1][node.col].isWall():
+                node.neighbors.append(grid[node.row + 1][node.col])
+
+    # Goes Up
+    if node.row > 0 and not grid[node.row - 1][node.col].isWall():  
+                node.neighbors.append(grid[node.row - 1][node.col])
+
+    # Goes Right
+    if node.col < node.total_cols - 1 and not grid[node.row][node.col + 1].isWall():
+                node.neighbors.append(grid[node.row][node.col + 1])
+
+    # Goes Left
+    if node.col > 0 and not grid[node.row][node.col - 1].isWall():  
+                node.neighbors.append(grid[node.row][node.col - 1])
+
+
 def astar(draw, grid, start, end):
+
     for row in grid:
         for node in row:
             updateNeighbor(node, grid)
-
 
     count = 0
     open_set = PriorityQueue()
@@ -66,21 +85,3 @@ def astar(draw, grid, start, end):
             curr.makeVisited()
 
     return False
-
-def updateNeighbor(node, grid):
-    node.neighbors = []
-    # Goes Down
-    if node.row < node.total_rows - 1 and not grid[node.row + 1][node.col].isWall():
-        node.neighbors.append(grid[node.row + 1][node.col])
-
-    # Goes Up
-    if node.row > 0 and not grid[node.row - 1][node.col].isWall():  
-        node.neighbors.append(grid[node.row - 1][node.col])
-
-    # Goes Right
-    if node.col < node.total_cols - 1 and not grid[node.row][node.col + 1].isWall():
-        node.neighbors.append(grid[node.row][node.col + 1])
-
-    # Goes Left
-    if node.col > 0 and not grid[node.row][node.col - 1].isWall():  
-        node.neighbors.append(grid[node.row][node.col - 1])

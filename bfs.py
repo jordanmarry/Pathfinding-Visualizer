bfc8dc622a238eceae1588a484dd4f2a2005f532
Jordan Marry <jmarry@terpmail.umd.edu>
2021-06-12 23:37:36 -0400

    BFS Algo is working properly and is implemented

--- bfs.py
+++ bfs.py
@@ -27,8 +27,41 @@ def reconstruct(prevNodes, curr, draw):
 def bfs(draw, grid, start, end):
     for row in grid:
         for node in row:
-            node.updateNeighbor(node, grid)
+            updateNeighbor(node, grid)
             
+    visited = []
+    queue = []
+    prevNodes = {}
     
+    curr = start
+    
+    visited.append(curr)
+    queue.append(curr)
+    
+    while queue:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        curr = queue.pop(0)
+        
+        if end in visited:
+            reconstruct(prevNodes, end, draw)
+            start.makeStart()
+            end.makeEnd()
+            return True
+        
+        for neighbor in curr.neighbors:
+            if neighbor not in visited:
+                prevNodes[neighbor] = curr
+                visited.append(neighbor)
+                queue.append(neighbor)
+                neighbor.makeNeighbor()
+                
+        draw()
+        
+        if curr != start:
+            curr.makeVisited()
+            
             
     

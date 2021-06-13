b0f060e623144c16b7c080fdc85f8a1915a1cd50
Jordan Marry <jmarry@terpmail.umd.edu>
2021-06-12 23:45:02 -0400

    Changes to Code added a helper class for algos

--- astaralgo.py
+++ astaralgo.py
@@ -1,38 +1,7 @@
 from queue import PriorityQueue
 import pygame
 
-
-def h(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return abs(x1-x2) + abs(y1-y2)
-
-
-def reconstruct(prevNodes, curr, draw):
-    while curr in prevNodes:
-        curr = prevNodes[curr]
-        curr.makePath()
-        draw()
-
-
-def updateNeighbor(node, grid):
-    node.neighbors = []
-    # Goes Down
-    if node.row < node.total_rows - 1 and not grid[node.row + 1][node.col].isWall():
-                node.neighbors.append(grid[node.row + 1][node.col])
-
-    # Goes Up
-    if node.row > 0 and not grid[node.row - 1][node.col].isWall():  
-                node.neighbors.append(grid[node.row - 1][node.col])
-
-    # Goes Right
-    if node.col < node.total_cols - 1 and not grid[node.row][node.col + 1].isWall():
-                node.neighbors.append(grid[node.row][node.col + 1])
-
-    # Goes Left
-    if node.col > 0 and not grid[node.row][node.col - 1].isWall():  
-                node.neighbors.append(grid[node.row][node.col - 1])
-
+from algoHelpers import *
 
 def astar(draw, grid, start, end):
 
--- bfs.py
+++ bfs.py
@@ -1,28 +1,6 @@
 import pygame
 
-def updateNeighbor(node, grid):
-    node.neighbors = []
-    # Goes Down
-    if node.row < node.total_rows - 1 and not grid[node.row + 1][node.col].isWall():
-                node.neighbors.append(grid[node.row + 1][node.col])
-
-    # Goes Up
-    if node.row > 0 and not grid[node.row - 1][node.col].isWall():  
-                node.neighbors.append(grid[node.row - 1][node.col])
-
-    # Goes Right
-    if node.col < node.total_cols - 1 and not grid[node.row][node.col + 1].isWall():
-                node.neighbors.append(grid[node.row][node.col + 1])
-
-    # Goes Left
-    if node.col > 0 and not grid[node.row][node.col - 1].isWall():  
-                node.neighbors.append(grid[node.row][node.col - 1])
-
-def reconstruct(prevNodes, curr, draw):
-    while curr in prevNodes:
-        curr = prevNodes[curr]
-        curr.makePath()
-        draw()
+from algoHelpers import *
 
 def bfs(draw, grid, start, end):
     for row in grid:

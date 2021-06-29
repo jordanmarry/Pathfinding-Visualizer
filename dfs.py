7fc2806181829e4f6d9bf964ee0c8a77325d7921
Jordan Marry <jordanmarryautumn@gmail.com>
2021-06-29 14:22:33 -0400

    DFS works

--- dfs.py
+++ dfs.py
@@ -6,3 +6,38 @@ def dfs(draw, grid, start, end):
     for row in grid:
         for node in row:
             updateNeighbor(node, grid)
+            
+    visited = []
+    queue = []
+    prevNodes = {}
+    
+    curr = start
+
+    queue.append(curr)
+    
+
+    while queue:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        curr = queue.pop()
+        
+        visited.append(curr)
+
+        if end in visited:
+            reconstruct(prevNodes, end, draw)
+            start.makeStart()
+            end.makeEnd()
+            return True
+        
+        for neighbor in curr.neighbors:
+            if neighbor not in visited:
+                prevNodes[neighbor] = curr
+                queue.append(neighbor)
+                neighbor.makeNeighbor()
+            
+        draw()
+        
+        if curr != start:
+            curr.makeVisited()

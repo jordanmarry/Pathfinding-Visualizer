8abe5a4eb3de7d773cc44948697ed67ae762345a
Jordan Marry <jordanmarryautumn@gmail.com>
2021-08-18 20:26:37 -0400

    Dijkstra's Algorithm Works

--- dijk.py
+++ dijk.py
@@ -6,3 +6,38 @@ def dijk(draw, grid, start, end):
     for row in grid:
         for node in row:
             updateNeighbor(node, grid)
+
+    visited = [start]
+    queue = [start]
+    prevNodes = {}
+
+    curr = start
+
+    while queue:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        curr = queue.pop(0)
+
+        if end in visited:
+            reconstructDij(prevNodes, end, draw)
+            start.makeStart()
+            end.makeEnd()
+            
+            return True
+        
+        for neighbor in curr.neighbors:
+            if neighbor not in visited:
+                if curr == start:
+                    prevNodes[neighbor] = (curr, 1)
+                else:
+                    prevNodes[neighbor] = (curr, prevNodes[curr][1] + neighbor.weight)
+                visited.append(neighbor)
+                queue.append(neighbor)
+                neighbor.makeNeighbor()
+
+        draw()
+        
+        if curr != start:
+            curr.makeVisited()
